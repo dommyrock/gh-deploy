b82c8debb71a76a296f0ff73d5bf91b098c97bd1
Dominik <32032778+dommyrock@users.noreply.github.com>
2020-04-08 00:44:59 +0200

    webapp --git featured 2x previews

--- src/components/SyntaxHighlighter/GithubFeatured/GithubFeaturedList.js
+++ src/components/SyntaxHighlighter/GithubFeatured/GithubFeaturedList.js
@@ -5,23 +5,43 @@ import "../../../githubHackatlon.css";
 
 const GithubFeaturedList = () => {
   return (
-    <div id="hackathon-div" className="featured-section" data-v-5aa19bde data-v-19509d32 data-v-2974ba77>
-      <div className="featured" data-v-5aa19bde>
-        <div className="featured__list featured__list--animated" data-v-5aa19bde>
-          {data.map(item => {
-            return <LinkButton {...{ link: item.link, title: item, description: item.description }} />;
-          })}
+    <>
+      <div id="hackathon-div" className="featured-section" data-v-5aa19bde data-v-19509d32 data-v-2974ba77>
+        <div className="featured" data-v-5aa19bde>
+          <div className="featured__list featured__list--animated" data-v-5aa19bde>
+            {data.map((item) => {
+              return <LinkButton {...{ link: item.link, title: item, description: item.description }} />;
+            })}
+          </div>
+          <div className="featured__list featured__list--alt featured__list--animated" data-v-5aa19bde>
+            {data.map((item) => {
+              return <LinkButton {...{ link: item.link, title: item, description: item.description }} />;
+            })}
+          </div>
         </div>
-        <div className="featured__list featured__list--alt featured__list--animated" data-v-5aa19bde>
-          {data.map(item => {
-            return <LinkButton {...{ link: item.link, title: item, description: item.description }} />;
-          })}
+        {/* <div className="square" data-v-dfc99e4e data-v-5aa19bde>
+          <div className="grid" data-v-dfc99e4e></div>
+        </div> */}
+      </div>
+      {/* 2x just for testing  */}
+      <div id="hackathon-div" className="featured-section" data-v-5aa19bde data-v-19509d32 data-v-2974ba77>
+        <div className="featured" data-v-5aa19bde>
+          <div className="featured__list featured__list--animated" data-v-5aa19bde>
+            {data.map((item) => {
+              return <LinkButton {...{ link: item.link, title: item, description: item.description }} />;
+            })}
+          </div>
+          <div className="featured__list featured__list--alt featured__list--animated" data-v-5aa19bde>
+            {data.map((item) => {
+              return <LinkButton {...{ link: item.link, title: item, description: item.description }} />;
+            })}
+          </div>
+        </div>
+        <div className="square" data-v-dfc99e4e data-v-5aa19bde>
+          <div className="grid" data-v-dfc99e4e></div>
         </div>
       </div>
-      {/* <div className="square" data-v-dfc99e4e data-v-5aa19bde> just drawn square ...
-        <div className="grid" data-v-dfc99e4e></div>
-      </div> */}
-    </div>
+    </>
   );
 };
 
@@ -86,5 +106,5 @@ const data = [
   27,
   28,
   29,
-  30
+  30,
 ];
