46ae7e0f01bac7b21239e8c8c328f6e049f0a19a
Dominik <32032778+dommyrock@users.noreply.github.com>
2020-04-11 19:09:20 +0200

    webapp- added hooks info /exmples if i get stuck

--- src/components/Layout/AppRouter.jsx
+++ src/components/Layout/AppRouter.jsx
@@ -27,4 +27,50 @@ export default AppRouter;
 //Router info https://reacttraining.com/react-router/web/api/BrowserRouter (browser router /hash router (for static files) should wrap whole App.js in index.js to work properly!)
 //could also use next.js routing https://nextjs.org/docs/routing/introduction
 
+//useEffect() ...runs after state update and screen repaint by browser
+//data fetching :https://www.robinwieruch.de/react-hooks-fetch-data
+/*React remembers the effect function you provided, and runs it after flushing changes to the DOM and letting the browser paint the screen.
+ Inside the scope of a single render, props and state stay the same. 
+ 
+ FETCHING  EXAMPLE with query saved to state;
+function Parent() {
+  const [query, setQuery] = useState('react');
+
+  // ✅ Preserves identity until query changes
+  const fetchData = useCallback(() => {
+    const url = 'https://hn.algolia.com/api/v1/search?query=' + query;
+    // ... Fetch data and return it ...
+  }, [query]);  // ✅ Callback deps are OK
+
+  return <Child fetchData={fetchData} />
+}
+
+function Child({ fetchData }) {
+  let [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData().then(setData);
+  }, [fetchData]); // ✅ Effect deps are OK
+
+  // ...
+}
+Since fetchData only changes inside Parent when its query state changes, our Child won’t refetch the data until it’s actually necessary for the app.
+*/
+
 //useRef() faq --https://reactjs.org/docs/hooks-faq.html#is-there-something-like-instance-variables
+// Like obj instance variable (mutable)
+/*Example
+ function Example() {
+  const [count, setCount] = useState(0);
+  const latestCount = useRef(count);
+
+  useEffect(() => {
+    // Set the mutable latest value
+    latestCount.current = count;
+    setTimeout(() => {
+      // Read the mutable latest value
+      console.log(`You clicked ${latestCount.current} times`);
+    }, 3000);
+  });
+  // ...
+  */
