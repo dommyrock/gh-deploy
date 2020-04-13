486e2f7d4bf9ce72cabb35d117857477ced14a34
Dominik <32032778+dommyrock@users.noreply.github.com>
2020-04-13 15:45:48 +0200

    webapp-added draggable.module.css

--- src/components/draggableComponent/DraggableLists.js
+++ src/components/draggableComponent/DraggableLists.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 // import uuidv4 from "uuidv4/v4"; guid library, dont have npm for it here
-
+import styles from "./draggable.module.css";
 const DraggableList = (dataArray) => {
   // debugger;
   console.log(dataArray);
@@ -48,18 +48,11 @@ const DraggableList = (dataArray) => {
   //#endregion
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+    <div className={styles.flex_container_main}>
       <DragDropContext onDragEnd={(result) => onDragEnd(result, columns, setColumns)}>
         {Object.entries(columns).map(([columnId, column], index) => {
           return (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-              key={columnId}
-            >
+            <div className={styles.flex_body_child} key={columnId}>
               <h2>{column.name}</h2>
               <div style={{ margin: 8 }}>
                 <Droppable droppableId={columnId} key={columnId}>
@@ -70,10 +63,8 @@ const DraggableList = (dataArray) => {
                         ref={provided.innerRef}
                         style={{
                           background: snapshot.isDraggingOver ? "lightblue" : "lightgrey",
-                          padding: 4,
-                          width: 250,
-                          minHeight: 500,
                         }}
+                        className={styles.flex_container}
                       >
                         {column.items.map((item, index) => {
                           return (
@@ -86,13 +77,17 @@ const DraggableList = (dataArray) => {
                                     {...provided.dragHandleProps}
                                     style={{
                                       userSelect: "none",
-                                      padding: 16,
-                                      margin: "0 0 8px 0",
-                                      minHeight: "50px",
-                                      backgroundColor: snapshot.isDragging ? "#263B4A" : "#456C86", //changes elements color onDrag
-                                      color: "white",
+                                      //   padding: 16,
+                                      //   margin: "0 0 8px 0",
+                                      //   minHeight: "50px",
+                                      // backgroundColor: snapshot.isDragging ? "#263B4A" : "#456C86", //changes elements color onDrag
+                                      backgroundImage: snapshot.isDragging
+                                        ? "linear-gradient(-35deg, rgb(248, 232, 92), rgb(219, 93, 125), rgb(95, 121, 191))"
+                                        : "linear-gradient(-35deg, rgb(248, 232, 92), rgb(219, 93, 125), rgb(139, 95, 191))",
+                                      color: snapshot.isDragging ? "lightgrey" : "white",
                                       ...provided.draggableProps.style,
                                     }}
+                                    className={styles.btn_gradient}
                                   >
                                     {item.content}
                                   </div>
